ef7a0fa9038cecba4ce1f78574cc361fe5f2c67b
Aromi s <79002496+Aromi-s@users.noreply.github.com>
2021-02-23 12:40:08 +0530

    Add files via upload

---
+++ evnorodd.py
@@ -0,0 +1,6 @@
+num=int(input("Enter a number"))
+mode=num%2
+if mode>0:
+	print("This is an odd number")
+else:
+    print("This is an even number")	
---
+++ keys.py
@@ -0,0 +1,6 @@
+k_keys=['wed','kill','Ammu']
+k_values=[5,6,7]
+print("orginal key list is:", str(k_keys))
+print("orginal value list is:" ,str(k_values))
+result=dict(zip(k_keys,k_values))
+print("resultant dictionary:"+str(result))
